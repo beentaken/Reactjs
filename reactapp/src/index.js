d206f0b9f059feeae961e60e8e9ea71bbd408d59
Wei Wen ANG <ANG_Wei_Wen@certisgroup.com>
2024-06-16 21:03:23 +0800

    2

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -1,35 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-//import Keycloak from 'keycloak-js';
-
-//const keycloak = new Keycloak('./keycloak.json');
-
-//keycloak
-//    .init({ onLoad: 'login-required' })
-//    .then(async auth => {
-//        if (auth) {
-//            console.error(auth);         
-//        }
-//    })
-//    .catch(error => { //keycloak errors only
-//        ReactDOM.render(
-//            <div>Authentication failed.</div>,
-//            document.getElementById("root")
-//        );
-//        console.error(error);
-//    });
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
