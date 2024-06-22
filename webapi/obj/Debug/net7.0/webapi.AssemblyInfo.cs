87c61c2673264542c8b79924f23d998c57b2158f
Wei Wen ANG <ANG_Wei_Wen@certisgroup.com>
2024-06-22 21:52:48 +0800

    remove module

--- webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
+++ webapi/obj/Debug/net7.0/webapi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f01f1e2358c95ceb160f58a2d2ecabef47ea2a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
